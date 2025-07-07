f3794da44052c6319bab812f755395407f56f50d
vytran1 <vy.tn171003@gmail.com>
2025-07-07 20:57:13 +0700

    Initial Commit

---
+++ DoAnThucTapService/src/main/java/com/thuctap/DoAnThucTapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thuctap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnThucTapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnThucTapServiceApplication.class, args);
+	}
+
+}
